a9fd62de2e78eb2c54e3bb5b16d1c61d88895204
Proladon <proladon@gmail.com>
2022-05-08 18:21:40 +0800

    global event

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,10 +1,29 @@
-#![cfg_attr(
-  all(not(debug_assertions), target_os = "windows"),
-  windows_subsystem = "windows"
-)]
+use tauri::Manager;
+
+// #![cfg_attr(
+//   all(not(debug_assertions), target_os = "windows"),
+//   windows_subsystem = "windows"
+// )]
+
+#[derive(serde::Deserialize)]
+struct EventType {
+  message: String,
+}
+
 
 fn main() {
   tauri::Builder::default()
+    .setup(|app| {
+      app.listen_global("click", |event| {
+        if let Some(payload) = event.payload() {
+          let variant1: EventType = serde_json::from_str(payload).unwrap();
+
+          // Print var1
+          println!("msg: {}", variant1.message);
+        }
+      });
+      Ok(())
+    })
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
 }
